bb2ed9ccc1c30ea61338a2cbd83fb022c0d99d77
ABPSLP15043 <abpsbktlessonplan@gmail.com>
2026-05-21 21:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if api_key:
 
     genai.configure(api_key=api_key)
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-pro")
 
     st.subheader("Teacher Input Section")
 
